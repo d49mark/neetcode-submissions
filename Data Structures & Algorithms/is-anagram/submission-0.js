1e07e1ee7d669ec63cc704274b9e1e555b9a8f6e
Dilip <d49web@gmail.com>
2026-09-19 18:05:28 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length!==t.length) return false;
+        const sCount={}
+        const tCount={}
+        for (const i in s){
+            sCount[s[i]]= 1+ (sCount[s[i]] || 0)
+            tCount [t[i]]=1+ (tCount[t[i]] || 0)
+        }
+        for(const key in sCount){
+            if(sCount [key] !== tCount[key]){
+                return false
+            }
+        }
+        return true
+    }
+}
